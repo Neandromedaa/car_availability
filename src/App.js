f67e297138acac8767b21a5eef93efedc339b9b9
Neandromedaa <andreyskrylev1@gmail.com>
2024-06-24 20:52:28 +0300

    fix localStorage state save

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const data = window.localStorage.getItem('MY_APP_STATE');
   const [pick, setPick] = useState(true);
   const [date, setDate] = useState(dayjs().format('DD.MM.YYYY'));
-  const [check, setCheck] = useState([false, false, false, false, false, false, false, false, false, false]);
-  
+  const [check, setCheck] = useState(data ? JSON.parse(data) : [false, false, false, false, false, false, false, false, false, false]);
+
+  useEffect(() => {
+    window.localStorage.setItem('MY_APP_STATE', JSON.stringify(check));
+  }, [check]);
+
   function arrCheck(index){
     let copy = Object.assign([], check);
     copy[index] = !check[index];
@@ -19,18 +25,9 @@ function App() {
   }
 
   const showCars = cars.map((car, index) => {
-    return <CarCard name={car.name} img={car.img} check={check[index]} index={index} changeState={arrCheck}/>
+    return <CarCard key={uuidv4()} name={car.name} img={car.img} check={check[index]} index={index} changeState={arrCheck}/>
   });
-
-  useEffect(() => {
-    window.localStorage.setItem('MY_APP_STATE', JSON.stringify(check));
-  }, [check]);
-
-  useEffect(() => {
-    const data = window.localStorage.getItem('MY_APP_STATE');
-    if ( data !== null ) setCheck(JSON.parse(data));
-  }, []);
-
+  
   return (
     <>
       <div className='app' id='capture'>
