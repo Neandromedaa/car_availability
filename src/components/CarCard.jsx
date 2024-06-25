3b7cfc3e37a85137f51489e793e343d8d7c6c9e1
Neandromedaa <andreyskrylev1@gmail.com>
2024-06-25 17:50:28 +0300

    css change

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -2,24 +2,24 @@ import React, { useEffect, useState } from 'react';
 import './style.scss'
 
 function CarCard({ name, img, check, index, changeState }) {
-    
-
-   
 
   return (
     <div className='card'>
         <div className='name'>{name}</div>
-        <img src={img} alt=''></img>
+        <div className='imgContainer' style={{backgroundImage: `url(${img})`}}/>
+        
         <div className='checkBox' onClick={() => changeState(index)}>
             {check 
                 ? 
-                <svg width="133" height="97" viewBox="0 0 133 97" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10 10L66.7101 79.145C74.7115 88.9009 89.6369 88.9009 97.6383 79.145L123 48.2222" stroke="#00FF29" strokeWidth="20" strokeLinecap="round"/>
+                <svg width="71" height="71" viewBox="0 0 71 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7 7L64 64M64 7L7 64" stroke="#FF0000" stroke-width="13" stroke-linecap="round"/>
                 </svg>
+
                 :
-                <svg width="113" height="113" viewBox="0 0 113 113" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10 10L103 103M103 10L10 103" stroke="#FF0000" strokeWidth="20" strokeLinecap="round"/>
+                <svg width="90" height="71" viewBox="0 0 90 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M83 7L38.3452 62.1953C36.344 64.6689 32.5721 64.6689 30.5709 62.1953L7 33.0606" stroke="#00FF29" stroke-width="13" stroke-linecap="round"/>
                 </svg>
+
             }
         </div>
     </div>
--- src/components/cars.js
+++ src/components/cars.js
@@ -1,40 +1,40 @@
 
 const cars = [
     {
-        name: 'Chevrolet Camaro White',
-        img: 'img/whiteCamaro.png'
-    },
-    {
-        name: 'Ford Mustang Shelby Grey',
-        img: 'img/greyMustang.png'
-    },
-    {
-        name: 'Chevrolet Camaro Yellow',
-        img: 'img/yellowCamaro.png'
+        name: 'Ford Mustang Red',
+        img: 'img/redMustang.png'
     },
     {
         name: 'Ford Mustang Shelby Green',
         img: 'img/greenMustang.png'
     },
     {
-        name: 'Chevrolet Camaro Graphite',
-        img: 'img/greyCamaro.png'
-    },
-    {
-        name: 'Ford Mustang Shelby Yellow',
-        img: 'img/yellowMustang.png'
-    },
-    {
-        name: 'Ford Mustang Shelby Black',
-        img: 'img/blackMustang.png'
+        name: 'Chevrolet Camaro White',
+        img: 'img/whiteCamaro.png'
     },
     {
         name: 'Ford Mustang Red GT',
         img: 'img/redGT.png'
     },
     {
-        name: 'Ford Mustang Red',
-        img: 'img/redMustang.png'
+        name: 'Chevrolet Camaro Graphite',
+        img: 'img/greyCamaro.png'
+    },
+    {
+        name: 'Ford Mustang Shelby Grey',
+        img: 'img/greyMustang.png'
+    },
+    {
+        name: 'Ford Mustang Shelby Black',
+        img: 'img/blackMustang.png'
+    },
+    {
+        name: 'Ford Mustang Shelby Yellow',
+        img: 'img/yellowMustang.png'
+    },
+    {
+        name: 'Chevrolet Camaro Yellow',
+        img: 'img/yellowCamaro.png'
     },
     {
         name: 'BMW 430i Сabriolet',
